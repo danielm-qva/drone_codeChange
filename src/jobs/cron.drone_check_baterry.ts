7908fa00b074009b396129f0c9097cf93dc461de
danielm-qva <jmastrapa96@gmail.com>
2023-01-18 02:35:35 +0100

    limit for 25 % baterry.

--- src/jobs/cron.drone_check_baterry.ts
+++ src/jobs/cron.drone_check_baterry.ts
@@ -5,7 +5,7 @@ import dronModels from '../models/dron.models';
 
 export const job_check_baterry = cron.schedule('30 * * * * *', async () => {
     
-    const listdron = await dronModels.find().where('baterry').gt(0).lte(65);
+    const listdron = await dronModels.find().where('baterry').gt(0).lte(25);
          listdron.forEach( async (item : any) => {
                if(item.state !== 'LOADING'){
                   const a = await dronModels.findByIdAndUpdate(item._id ,{'state': "LOADING"});
